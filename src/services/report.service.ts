48d5759b8814e99d062a93f0ab3a9a95643e3f5b
Mukesh015 <rg37090@gmail.com>
2026-04-12 10:07:48 +0530

    reverted

--- src/services/report.service.ts
+++ src/services/report.service.ts
@@ -72,10 +72,8 @@ export const getOfflineReport = async (
 
     const data = paginated.map((r: any) => {
 
-        const timeout = tankTimeoutMap.get(r.tank_no) || 15;
-        const adjustedGap = r.gap_min - timeout;
-        const hours = Math.floor(adjustedGap / 60);
-        const minutes = adjustedGap % 60;
+        const hours = Math.floor(r.gap_min / 60);
+        const minutes = r.gap_min % 60;
 
         return {
             tank_no: r.tank_no,
